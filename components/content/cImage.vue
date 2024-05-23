0df01ece99d233eb6cae4066516af50fba1d781f
woxedev <erenwoxedemir@gmail.com>
2024-05-23 19:15:32 +0300

    component: Custom image component to display alt text under the image

---
+++ components/content/cImage.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <img class="mb-2" :src="src" :alt="alt" />
+      <p class="mt-0 text-xs text-center text-neutral-500">
+         {{ alt }}
+      </p>
+   </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+   src: string;
+   alt?: string;
+}>();
+</script>
